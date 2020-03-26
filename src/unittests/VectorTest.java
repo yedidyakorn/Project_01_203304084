47ccc0461cef5a05572148cac790b456dca9f061
ידידיה קורן <ykorn@g.jct.ac.il>
2020-03-26 03:08:34 +0200

    test class-midlle of VectorTest

--- src/unittests/VectorTest.java
+++ src/unittests/VectorTest.java
@@ -1,7 +1,6 @@
 package unittests;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 import primitives.Point3D;
 import primitives.Vector;
 
@@ -14,6 +13,11 @@ import static primitives.Util.isZero;
  * * @author Yedidya Korn & Eliezer Horowitz
  */
 public class VectorTest {
+
+    Vector v1 = new Vector(1, 2, 3);
+    Vector v2 = new Vector(-2, -4, -6);
+    Vector v3 = new Vector(0, 3, -2);
+
 
     @Test
     public void testCtor(){
@@ -51,6 +55,12 @@ public class VectorTest {
      */
     @org.junit.Test
     public void dotProduct() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: dotProduct() for orthogonal vectors test
+        assertTrue("ERROR: dotProduct() for orthogonal vectors is not zero",isZero(v1.dotProduct(v3)));
+
+        // TC02: There is a simple dotProduct test
+        assertTrue("ERROR: dotProduct() wrong value",isZero(v1.dotProduct(v2) + 28));
     }
 
     /**
@@ -58,6 +68,29 @@ public class VectorTest {
      */
     @org.junit.Test
     public void crossProduct() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: crossProduct() for parallel vectors test
+        try { // test zero vector
+            v1.crossProduct(v2);
+            fail("ERROR: crossProduct() for parallel vectors does not throw an exception");
+        } catch (Exception e) {  }
+
+        // TC02: There is a simple crossProduct test
+        Vector vr = v1.crossProduct(v3);
+        assertTrue("ERROR: crossProduct() wrong result length",isZero(vr.length() - v1.length() * v3.length()));
+
+        // TC03: crossProduct() orthogonal to its operands test
+        assertTrue("ERROR: crossProduct() result is not orthogonal to its operands",isZero(vr.dotProduct(v1)) || !isZero(vr.dotProduct(v3)));
+    }
+
+    /**
+     * Test method for {@link primitives.Vector#lengthSquared (primitives.Vector)}.
+     */
+    @org.junit.Test
+    public void lengthSquared() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: There is a simple single length test..
+        assertTrue("ERROR: lengthSquared() wrong value",isZero(v1.lengthSquared()-14));
     }
 
     /**
@@ -65,12 +98,9 @@ public class VectorTest {
      */
     @org.junit.Test
     public void length() {
-        Vector v1 = new Vector(1, 2, 3);
-        Vector v2 = new Vector(-2, -4, -6);
-        Vector v3 = new Vector(0, 3, -2);
-        assert("ERROR: length() wrong value",new Vector(0, 3, 4).length(),5);
-        if (!isZero(new Vector(0, 3, 4).length() - 5))
-            fail("ERROR: length() wrong value");
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: There is a simple single length test..
+        assertTrue("ERROR: length() wrong value",isZero(new Vector(0, 3, 4).length()-5));
     }
 
     /**
@@ -80,15 +110,5 @@ public class VectorTest {
     public void normalize() {
     }
 
-    /**
-     * Test method for {@link primitives.Vector#lengthSquared (primitives.Vector)}.
-     */
-    @org.junit.Test
-    public void lengthSquared() {
-        Vector v1 = new Vector(1, 2, 3);
-        assert("ERROR: lengthSquared() wrong value",v1.lengthSquared(),14);
-        assertEquals("ERROR: lengthSquared() wrong value",!isZero(v1.lengthSquared()-14));
-        ass
-    }
 
 }

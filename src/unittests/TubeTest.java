8cfda44c6fd711f421f00b680284c92b3e5847c2
ידידיה קורן <ykorn@g.jct.ac.il>
2020-03-31 15:09:41 +0300

    tube test

--- src/unittests/TubeTest.java
+++ src/unittests/TubeTest.java
@@ -26,9 +26,9 @@ public class TubeTest {
         // ============ Equivalence Partitions Tests ==============
         // TC01: There is a simple single normal test..
         Tube t1 = new Tube(new Ray(new Point3D(5, 5, 5), new Vector(1, 0, 0)), 4);
-        assertEquals("ERROR- the function does not return a true normal vector", new Vector(0, 0, 1).normalize(), t1.getNormal(new Point3D(1, 5, 9)));
+        assertEquals("ERROR- the function does not return a true normal vector", new Vector(0, 0, 5).normalize(), t1.getNormal(new Point3D(1, 5, 9)));
 
-        // TC01: There is a simple single normal test for a point that is on the orthogonal to the tubes rays point..
+        // TC02: There is a simple single normal test for a point that is on the orthogonal to the tubes rays point..
         Tube t2 = new Tube(new Ray(new Point3D(0, 0, 0), new Vector(0, 2, 0)), 3);
         assertEquals("ERROR- the function does not return a true normal vector", new Vector(3, 0, 0).normalize(), t2.getNormal(new Point3D(3, 0, 0)));
 
